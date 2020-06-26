83a3121461053912c658cfec531f57efc0accf32
Dian Nandiwardhana <prabuwardhana03@yahoo.com>
2020-06-27 00:36:28 +0900

    Add BankTransactionSimulation.EventArgs console project

---
+++ CodingGym12.BankTransactionSimulation.EventArgs/Account.cs
@@ -0,0 +1,39 @@
+namespace CodingGym12.BankTransactionSimulation.EventArgs
+{
+    public class TransactionEventArgs : EventArgs
+    {
+        public readonly string msg;
+
+        public TransactionEventArgs(string msg)
+        {
+            this.msg = msg;
+        }
+    }
+
+    public class Account
+    {
+        public int Balance { get; set; }
+
+        /* no longer need to define a custom delegate type at all, so we can remove the delegate declaration */
+        public event EventHandler<TransactionEventArgs> Transaction;
+
+        public void Withdrawal(int amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                Transaction?.Invoke(this, new TransactionEventArgs($"You withdrew Rp. {amount:N0}. Balance: Rp. {Balance:N0}"));
+            }
+            else
+            {
+                Transaction?.Invoke(this, new TransactionEventArgs($"Transaction FAILED! You are not allowed to withdraw more than : Rp. {Balance:N0}"));
+            }
+        }
+
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+            Transaction?.Invoke(this, new TransactionEventArgs($"You deposited: Rp. {amount:N0}. Balance: Rp. {Balance:N0}"));
+        }
+    }
+}
---
+++ CodingGym12.BankTransactionSimulation.EventArgs/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CodingGym12.BankTransactionSimulation.EventArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* First, make an Account object */
+            Account acc = new Account();
+
+            #region Registering the handler
+            /* Register event handlers */
+            acc.Transaction += OnTransactionEvent;
+            acc.Transaction += OnTransactionEvent2;
+
+            /* Register event handlers as anonymous methods. */
+            // acc.Transaction += delegate(object sender, TransactionEventArgs e)
+            // {
+            //    Console.WriteLine(e.msg);
+            // };
+
+            /* Register event handlers using lambda expression */
+            // acc.Transaction += (object sender, TransactionEventArgs e) => Console.WriteLine(e.msg);    
+            #endregion            
+
+            #region Trigger events            
+            /* Make a transaction (this will trigger the events) */
+            bool contd = true;
+
+            do
+            {
+                Console.Write("Please select a transaction or quit [1. deposit, 2. withdrawal, 3. quit]: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Deposit amount: ");
+                        int deposit = int.Parse(Console.ReadLine());                        
+                        acc.Deposit(deposit);
+                        break;
+                    case 2:
+                        Console.Write("Withdrawal amount: ");
+                        int withdraw = int.Parse(Console.ReadLine());                        
+                        acc.Withdrawal(withdraw);
+                        break;
+                    case 3:
+                        contd = false;
+                        break;
+                    default:
+                        break;
+                }
+
+                #region Unregister the OnTransactionEvent2 handler
+                acc.Transaction -= OnTransactionEvent2;
+                #endregion
+            }
+            while (contd);
+            #endregion
+        }
+
+        /* This is the target for incoming events */
+        public static void OnTransactionEvent(object sender, TransactionEventArgs e) => Console.WriteLine(e.msg);
+
+        public static void OnTransactionEvent2(object sender, TransactionEventArgs e) => Console.WriteLine(e.msg.ToUpper());
+    }
+}
